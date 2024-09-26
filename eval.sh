fbec06965dd9daa74623eb0af874ba881aa9df3b
zee-tek <zeeshanali6091@gmail.com>
2024-09-26 03:37:11 -0400

    added autofs logic

--- eval.sh
+++ eval.sh
@@ -16,17 +16,30 @@ app_chk=$?
 dnf repolist --enabled -q|egrep -v '^rhel|^repo'|grep -i base&>/dev/null
 base_chk=$?
 
-usr_shell=` getent passwd sarah|awk -F ':' '{print $NF}'|grep nologin`
+usr_shell=` getent passwd sarah|awk -F ':' '{print $NF}'|grep nologin` &>/dev/null
 usr_chk=$?
 
-getent group admins|grep sarah
+getent group admins|grep sarah &>/dev/null
 chk_usr1=$?
 
-getent group admins|grep harry
+getent group admins|grep harry &>/dev/null
 chk_usr2=$?
 
-getent group admins|grep natasha
+getent group admins|grep natasha &>/dev/null
 chk_usr3=$?
+
+u_mask=`su - natasha -c "umask"`
+
+sudo_pr=`grep "^%admins" /etc/sudoers`
+sudo_chk=$?
+
+
+
+if [ $u_mask == 0022 ];then
+        echo "Pass"
+else
+        echo "Fail"
+fi
 
 echo "Checking Ip"....................
 
@@ -70,4 +83,35 @@ else
 
 fi
 
-if [ $usr_chk -eq 0 ] 
+if [ $chk_usr1 -eq 0 ];then
+	echo "Pass"
+else
+	echo "Pass"
+fi
+
+if [ $chk_usr2 -eq 0 ];then
+        echo "Pass"
+else
+        echo "Pass"
+fi
+
+if [ $chk_usr3 -eq 0 ];then
+        echo "Pass"
+else
+        echo "Pass"
+fi
+
+if [ $u_mask == 0022 ];then
+        echo "Pass"
+else
+        echo "Fail"
+fi
+
+if [ $sudo_chk -eq 0 ];then
+        echo "Pass"
+else
+        echo "Pass"
+fi
+
+echo "============================"
+echo "Question 4"
