494d4e65b5d8df7278c0bae3cb9d90ffe6217953
zee-tek <zeeshanali6091@gmail.com>
2024-09-28 04:09:55 -0400

    added autofs logic

--- web-test.sh
+++ web-test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+: ' This Script evaluates following Tasks
+   - Check if web container is running or not
+   - Check if Selinux fixed and webserver on VM is running or not
+   - Check if websites running on container and vm are accessible or not
+   - Check if Container and VM webservers Ports are open on firewall or not
+   - Check if autofs filesystem is mounted or not
+  '
+
+################################################################ 
 
 container_chk=`curl localhost:8085 2>/dev/null`
 selinux_chk=`curl localhost:82 2>/dev/null`
@@ -15,51 +24,58 @@ mnt_st=$?
 
 if [ "$container_chk" == "Hello From myweb1 Container" ];then
 
-     echo "Pass: WebSite running inside container is accessible "
+	echo
+	echo -e "\e[32m Pass: Container Website is accessible \e[0m\n"
 
 else
-
-    echo "Fail"
+    echo
+    echo -e "\e[31m Fail: Container Website is not accessible \e[0m\n"
 fi
 
 
 
 if [ "$selinux_chk" == "Practicing RHCSA9" ];then
 
-     echo "Pass: Selinux is good, WebSite hosting on VM is accessible"
+
+	echo -e "\e[32m Pass: Selinux is good, WebSite hosting on VM is accessible \e[0m\n"
 
 else
 
-    echo "Fail"
+    echo -e "\e[31m Fail: Selinux is good, WebSite hosting on VM is not accessible \e[0m\n"
 fi
 
 
 if [ $selinux_port -eq 0 ];then
 
-     echo "Pass: Selinux webserver Port is good."
 
+	echo -e "\e[32m Pass: Selinux webserver Port is good. \e[0m\n"
 else
 
-    echo "Fail"
+    echo -e "\e[31m Fail: Selinux webserver Port is not good. \e[0m\n"
 fi
 
 
 if [ $container_port -eq 0 ];then
 
-     echo "Pass: Container webserver Port is good."
+
+	echo -e "\e[32m Pass: Container webserver Port is good. \e[0m\n"
 
 else
 
-    echo "Fail"
+    echo -e "\e[31m Fail: Container webserver Port is not good. \e[0m\n"
 fi
 
 
 
 if [ $mnt_st -eq 0  ];then
 
-     echo "Pass: Autofs filesystem is mounted"
+
+	echo -e "\e[32m Pass: Autofs filesystem is mounted \e[0m\n"
 
 else
 
-    echo "Fail"
+    echo -e "\e[31m Fail: Autofs filesystem is not mounted \e[0m\n"
 fi
+
+
+Author: Zeeshan Ali
